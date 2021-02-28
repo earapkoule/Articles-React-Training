cb7956811197fa67d37276bbc19253ece1fe4642
atha-efthymi <79157098+atha-efthymi@users.noreply.github.com>
2021-02-28 13:01:56 +0200

    Add files via upload
    Homepage adjusted

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import RandomPost from "../components/RandomPost/RandomPost"
+import Banner from "../components/Banners/Banners"
 
 
 const HomePage = () => {
     return (
         <div>
             <h1>Homepage</h1>
+            <Banner/>
             <RandomPost></RandomPost>
         </div>)
 }
