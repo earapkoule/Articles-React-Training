9c7faf0c277d640f57cd3975642c92db5e03da3a
Charalampos AMARANTIGOS <Charalampos.AMARANTIGOS>
2021-03-01 09:45:26 +0200

    merged successful

--- src/App.js
+++ src/App.js
@@ -12,40 +12,7 @@ import ContactPage from './pages/ContactPage';
 import { theme } from './config';
 import { ThemeProvider } from './providers/ThemeProvider';
 
-<<<<<<< HEAD
-
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <div>
-          {/*
-            auto tha ginei to navbar component
-          */}
-          <NavBar></NavBar>
-
-          <hr />
-
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-
-            <Route path="/:category" render={(props) => { (<BlogPage category={props.match.params.category} />) }}>
-            </Route>
-            <Route path="/:category/:id"  >
-
-            </Route>
-            <Route path="/contact">
-
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </ThemeProvider>
-=======
-const BlogPage = () => {
-  return <h1>Hello blogpage</h1>;
-};
+ 
 const categoryPaths = [
   "/fashion",
   "/lifestyle",
@@ -57,28 +24,28 @@ const postPaths = categoryPaths.map((path) => path + "/:id");
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar />
-
-        <Switch>                                           
-          <Route exact path="/" component={HomePage} />
-          <Route path="/contact"></Route>
-          <Route
-            exact
-            path={categoryPaths}
-            render={(props) => {
-              return <BlogPage category={props.match.params.category} />;
-            }}
-          ></Route>
-          <Route exact path={postPaths}></Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
->>>>>>> 155f5b751d3a895774e8160500d65628eac9c9d3
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/contact"></Route>
+            <Route
+              exact
+              path={categoryPaths}
+              render={(props) => {
+                return <BlogPage category={props.match.params.category} />;
+              }}
+            ></Route>
+            <Route exact path={postPaths}></Route>
+            <Route path="*">
+              
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
