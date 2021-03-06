3528f8762d39c766f5ff797dea43920519733392
atha-efthymi <79157098+atha-efthymi@users.noreply.github.com>
2021-03-06 17:19:41 +0200

    Add files via upload

--- src/components/Banners/Banners.js
+++ src/components/Banners/Banners.js
@@ -22,7 +22,7 @@ const Banner = () => {
         {tileData.map((tile) => (
           <GridListTile key={tile.social_image}>
             <Link to={`/posts/${tile.id}`}>
-              <img style={{width: '100%'}} src={tile.social_image} alt={tile.title} />
+              <img style={{width: '100%', height: '100%'}} src={tile.social_image} alt={tile.title} />
               <GridListTileBar
                 title={tile.title}
                 classes={{
