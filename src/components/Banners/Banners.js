62da3bbb9747e996146e8996189daef69da3caf7
atha-efthymi <79157098+atha-efthymi@users.noreply.github.com>
2021-03-06 17:16:26 +0200

    Add files via upload
    Banner-links

--- src/components/Banners/Banners.js
+++ src/components/Banners/Banners.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { GET_ALL_POSTS } from '../../utils/apiRoutes';
 import { useFetch } from '../../utils/fetchHook';
+import { Link } from "react-router-dom";
 import "./Banners.module.css";
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
@@ -20,14 +21,16 @@ const Banner = () => {
       <GridList className="gridList" cols={3}>
         {tileData.map((tile) => (
           <GridListTile key={tile.social_image}>
-            <img src={tile.social_image} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              classes={{
-                root: "titleBar",
-                title: "title",
-              }}
-            />
+            <Link to={`/posts/${tile.id}`}>
+              <img style={{width: '100%'}} src={tile.social_image} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                classes={{
+                  root: "titleBar",
+                  title: "title",
+                }}
+              />
+            </Link>
           </GridListTile>
         ))}
       </GridList>
