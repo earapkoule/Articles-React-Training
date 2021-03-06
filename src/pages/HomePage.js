a2407c8706632e8e2d9c0dc32c6f45505af9121b
atha-efthymi <79157098+atha-efthymi@users.noreply.github.com>
2021-03-06 12:07:07 +0200

    Add files via upload
    HomePage adjusted

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,21 +2,23 @@ import React, { Fragment } from "react";
 import RandomPost from "../components/RandomPost/RandomPost";
 import PopularPosts from "../components/PopularPosts/PopularPosts";
 import Banner from "../components/Banners/Banners"
+import MainArticles from "../components/MainArticles/MainArticles"
 import Grid from '@material-ui/core/Grid';
 
 const HomePage = () => {
   return (
     <Fragment>
-      
       <Banner/>
-      <Grid container spacing={24}>
+      <Grid container>
+        <Grid item xs={1}></Grid>
         <Grid item xs={8} >
-            <h1>Main topics</h1>
-          </Grid>
-          <Grid item xs={4} >
-            <PopularPosts />
-            <RandomPost />
+          <MainArticles />
         </Grid>
+        <Grid item xs={2} >
+          <PopularPosts />
+          <RandomPost />
+        </Grid>
+        <Grid item xs={1}></Grid>
       </Grid>
     </Fragment>
   );
