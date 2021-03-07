5822e038b3d75e0dddb4583702ae2197359b1536
atha-efthymi <79157098+atha-efthymi@users.noreply.github.com>
2021-03-07 19:59:11 +0200

    Add files via upload

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from "react";
 import { useLocation } from "react-router";
 import PopularPosts from "../components/PopularPosts/PopularPosts";
-
+import RandomPost from "../components/RandomPost/RandomPost";
+ import MainArticles from "../components/MainArticles/MainArticles";
+import Grid from "@material-ui/core/Grid";
 const BlogPage = () => {
   const location = useLocation();
   const category = location.pathname.substring(1);
   return (
+   
     <Fragment>
-      <h1>BlogPage</h1>
-      <PopularPosts tag={category} />
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <MainArticles tag={category} />
+        </Grid>
+        <Grid item xs={4}>
+          <PopularPosts tag={category} />
+          <RandomPost />
+        </Grid>
+        <Grid item xs={1}></Grid>
+      </Grid>
     </Fragment>
   );
 };
