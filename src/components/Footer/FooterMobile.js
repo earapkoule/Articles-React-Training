7eb8c16d28194cb6e6d75c489e6cffbb314a9f93
5841@oo88git push --set-upstream https://gitlab.example.com/harisamarad/gamesworld-backend masterWolf <harisamarad@gmail.com>
2021-03-07 21:32:41 +0200

    footer fixed

--- src/components/Footer/FooterMobile.js
+++ src/components/Footer/FooterMobile.js
@@ -1,46 +1,63 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { Divider } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    footer: {
-        minHeight: '250px',
-        boxShadow: '0px 1px 10px rgba(0, 0, 0, 1)',
-        color: theme.palette.text.primary,
-        display: 'flex',
-        flexDirection: 'column',
-        padding: '30px 21vw',
-        '@media (max-width:960px)': {
-            padding: '20px',
-        },
+  footer: {
+    minHeight: "50px",
+    boxShadow: "0px 1px 10px rgba(0, 0, 0, 1)",
+
+    backgroundColor: theme.palette.primary.main,
+    display: "flex",
+    flexDirection: "column",
+    padding: "30px 21vw",
+    "@media (max-width:960px)": {
+      padding: "20px",
     },
+  },
+  innerFooterText: {
+    color: theme.palette.text.header,
+  },
 }));
 
 const useStylesFooterTitle = makeStyles((theme) => ({
-    title: {
-        fontWeight: theme.typography.titleFontWeight,
-        fontSize: theme.typography.titleFontSize,
-        borderBottom: '2px solid' + theme.palette.text.primary,
-        paddingBottom: '5px',
-    },
+  title: {
+    fontWeight: theme.typography.titleFontWeight,
+    fontSize: theme.typography.titleFontSize,
+    borderBottom: "2px solid" + theme.palette.text.primary,
+    paddingBottom: "5px",
+  },
 }));
 
 const FooterTitle = ({ title }) => {
-    const classes = useStylesFooterTitle();
-    return <Typography className={classes.title}>{title}</Typography>;
+  const classes = useStylesFooterTitle();
+  return <Typography className={classes.title}>{title}</Typography>;
 };
 
 const FooterMobile = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.footer}>
-            <Typography variant="body2" align="left">
-                {`Copyright © ${new Date().getFullYear()}. All rights reserved.`}
-            </Typography>
-        </div>
-    );
+  return (
+    <div className={classes.footer}>
+      <Typography
+        variant="body2"
+        align="left"
+        className={classes.innerFooterText}
+      >
+        {`Team 1`}
+        <Divider />
+        {`Amarantigos Charalambos - DG 1`}
+        <Divider />
+        {`Arapkoule Eirini - DG 13`}
+        <Divider />
+        {`Eythimiou Athanasios - DG 8`}
+        <Divider />
+        {`Copyright © ${new Date().getFullYear()}. All rights reserved.`}
+      </Typography>
+    </div>
+  );
 };
 
 export default FooterMobile;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,46 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { Divider } from "@material-ui/core";
 
-const useStyles = makeStyles((_theme) => ({
-    footer: {
-        minHeight: '250px',
-        boxShadow: '0px 1px 10px rgba(0, 0, 0, 1)',
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    minHeight: "50px",
+    boxShadow: "0px 1px 10px rgba(0, 0, 0, 1)",
+    backgroundColor: theme.palette.primary.main,
+  },
+  innerFooter: {
+    margin: "0px 15vw",
+    padding: "20px 0",
+
+    "@media (max-width:960px)": {
+      margin: "0",
     },
-    innerFooter: {
-        margin: '0px 15vw',
-        padding: '20px 0',
-        '@media (max-width:960px)': {
-            margin: '0',
-        },
-    },
+  },
+  innerFooterText: { color: theme.palette.text.header },
 }));
 
 const useStylesFooterTitle = makeStyles((theme) => ({
-    title: {
-        borderBottom: '2px solid' + theme.palette.text.primary,
-        paddingBottom: '5px',
-    },
+  title: {
+    borderBottom: "2px solid" + theme.palette.text.primary,
+    paddingBottom: "5px",
+  },
 }));
 
 const FooterTitle = ({ title }) => {
-    const classes = useStylesFooterTitle();
-    return <Typography className={classes.title}>{title}</Typography>;
+  const classes = useStylesFooterTitle();
+  return <Typography className={classes.title}>{title}</Typography>;
 };
 
 const Footer = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.footer}>
-            <div className={classes.innerFooter}>
-                <Typography variant="body2" align="left">
-                    {`Copyright © ${new Date().getFullYear()}. All rights reserved.`}
-                </Typography>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.footer}>
+      <div className={classes.innerFooter}>
+        <Typography
+          variant="span"
+          align="left"
+          className={classes.innerFooterText}
+        >
+          {`Team 1`}
+          <Divider />
+          {`Amarantigos Charalambos - DG 1`}
+          <Divider />
+          {`Arapkoule Eirini - DG 13`}
+          <Divider />
+          {`Eythimiou Athanasios - DG 8`}
+          <Divider />
+          {`Copyright © ${new Date().getFullYear()}. All rights reserved.`}
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
